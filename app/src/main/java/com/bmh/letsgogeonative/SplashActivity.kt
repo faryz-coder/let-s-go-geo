cc288155c229aa563c565ba99b519710b371628c
faryz-coder <faryz.coder@gmail.com>
2023-12-22 07:17:59 +0800

    add splash screen

---
+++ app/src/main/java/com/bmh/letsgogeonative/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.bmh.letsgogeonative
+
+import android.content.Intent
+import android.graphics.drawable.AnimatedImageDrawable
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.bmh.letsgogeonative.databinding.SplashScreenBinding
+import com.bmh.letsgogeonative.ui.login.SignInActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: SplashScreenBinding
+
+    @OptIn(DelicateCoroutinesApi::class)
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = SplashScreenBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(binding.root)
+        (binding.imageView4.drawable as AnimatedImageDrawable).start()
+
+        GlobalScope.launch {
+            delay(3000)
+            val intent = Intent(this@SplashActivity, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
